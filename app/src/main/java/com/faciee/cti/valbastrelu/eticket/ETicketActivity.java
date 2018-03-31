39a0a7df2f0dd8cfb48afeef2bec2cf2837ee17d
valbastrelu <viorel.albastrelu@footasylum.com>
2018-03-31 16:37:52 +0300

    init commit

---
+++ app/src/main/java/com/faciee/cti/valbastrelu/eticket/ETicketActivity.java
@@ -0,0 +1,13 @@
+package com.faciee.cti.valbastrelu.eticket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ETicketActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_eticket);
+	}
+}
