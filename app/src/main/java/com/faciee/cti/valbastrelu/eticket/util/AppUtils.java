66d18d970336bf13d77c52758d7b1b274324b9d3
valbastrelu <viorel.albastrelu@footasylum.com>
2018-04-30 15:39:24 +0300

    added chatbot activity

---
+++ app/src/main/java/com/faciee/cti/valbastrelu/eticket/util/AppUtils.java
@@ -0,0 +1,22 @@
+package com.faciee.cti.valbastrelu.eticket.util;
+
+import android.os.Environment;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class AppUtils {
+	//check SD card availability
+	public static boolean isSDCARDAvailable(){
+		return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)? true :false;
+	}
+	//copying the file
+	public static void copyFile(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int read;
+		while((read = in.read(buffer)) != -1){
+			out.write(buffer, 0, read);
+		}
+	}
+}
