639b6b1ccabe3e4d523a6fe349233258985bd1bd
valbastrelu <viorel.albastrelu@footasylum.com>
2018-05-13 01:39:16 +0300

    redesign on traseu row

--- app/src/main/java/com/faciee/cti/valbastrelu/eticket/ui/common/adapters/TraseuRVAdapter.java
+++ app/src/main/java/com/faciee/cti/valbastrelu/eticket/ui/common/adapters/TraseuRVAdapter.java
@@ -8,26 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.faciee.cti.valbastrelu.eticket.R;
 import com.faciee.cti.valbastrelu.eticket.ui.bus.BusActivity;
 import com.faciee.cti.valbastrelu.eticket.ui.bus.FrgTb02TraseuStatii;
+import com.faciee.cti.valbastrelu.eticket.ui.model.Traseu;
 
 import java.util.ArrayList;
 
 public class TraseuRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 	private static final String TAG = "TraseuRecyclerViewAdapt";
 	
-	private ArrayList<String> mTimp;
-	private ArrayList<String> mTraseu;
-	Context mContext;
+	private ArrayList<Traseu> mTrasee;
+	private Context mContext;
 	
-	public TraseuRVAdapter(Context mContext, ArrayList<String> mTimp, ArrayList<String> mTraseu) {
+	public TraseuRVAdapter(Context mContext, ArrayList<Traseu> lTrasee) {
 		this.mContext = mContext;
-		this.mTimp = mTimp;
-		this.mTraseu = mTraseu;
+		this.mTrasee = lTrasee;
 	}
 	
 	@Override
@@ -40,25 +40,30 @@ public class TraseuRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 	public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 		Log.d(TAG, "onBindViewHolder: called.");
 		TraseuViewHolder viewHolder = (TraseuViewHolder) holder;
-		viewHolder.timeStamp.setText(mTimp.get(position));
-		viewHolder.traseu.setText(mTraseu.get(position));
+		viewHolder.icon.setImageResource(mTrasee.get(position).getTransportType().getTransportTypeIcon());
+		viewHolder.timeStamp.setText(mContext.getString(R.string.ora_traseu_fmt,
+				mTrasee.get(position).getOra()));
+		viewHolder.traseu.setText(mContext.getString(R.string.nr_traseu_fmt, mTrasee.get(position).getNrTraseu()));
 		viewHolder.infoBtn.setOnClickListener(v -> {
-			Toast.makeText(mContext, mTimp.get(position) + " - "+  mTraseu.get(position), Toast.LENGTH_SHORT).show();
+			Toast.makeText(mContext, mTrasee.get(position).getOra() + " - "
+					+  mTrasee.get(position).getNrTraseu(), Toast.LENGTH_SHORT).show();
 		});
 	}
 	
 	@Override
 	public int getItemCount() {
-		return mTraseu.size();
+		return mTrasee.size();
 	}
 	
 	public class TraseuViewHolder extends RecyclerView.ViewHolder{
+		ImageView icon;
 		TextView timeStamp;
 		TextView traseu;
 		Button infoBtn;
 		
 		public TraseuViewHolder(View itemView) {
 			super(itemView);
+			icon = itemView.findViewById(R.id.traseuTypeIcon);
 			timeStamp = itemView.findViewById(R.id.time_stamp);
 			traseu = itemView.findViewById(R.id.traseu_no);
 			infoBtn = itemView.findViewById(R.id.infoBtn);
